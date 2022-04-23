e755782a6b56f27d25294186431ea58f8d9ecc42
jaehyeon2 <wol5971@gmail.com>
2022-04-23 21:49:38 +0900

    thymeleafEx2/3

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,14 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -13,4 +18,37 @@ public class ThymeleafExController {
         model.addAttribute("data", "타임리프 예제입니다.");
         return "thymeleafEx/thymeleafEx01";
     }
+
+    @GetMapping("/ex02")
+    public String thymeleafExample02(Model model){
+        ItemDTO itemDTO=new ItemDTO();
+        itemDTO.setItemDetail("상품 상세 설명");
+        itemDTO.setItemNm("테스트 상품1");
+        itemDTO.setPrice(10000);
+        itemDTO.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDTO", itemDTO);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model){
+
+        List<ItemDTO> itemDTOList = new ArrayList<>();
+
+        for(int i=1; i<=10; i++){
+            ItemDTO itemDTO=new ItemDTO();
+            itemDTO.setItemDetail("상품 상세 설명");
+            itemDTO.setItemNm("테스트 상품"+i);
+            itemDTO.setPrice(1000*i);
+            itemDTO.setRegTime(LocalDateTime.now());
+
+            itemDTOList.add(itemDTO);
+        }
+
+        model.addAttribute("itemDTOList", itemDTOList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+
+
 }
