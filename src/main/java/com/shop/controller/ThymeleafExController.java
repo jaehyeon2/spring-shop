017bddb8320b6a474d20fd35b3f22928199a6a1b
jaehyeon2 <wol5971@gmail.com>
2022-04-25 20:53:47 +0900

    thymeleafEx7

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -81,7 +81,7 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx06";
     }
 
-    @GetMapping("/ex07")
+    @GetMapping(value = "/ex07")
     public String thymeleafExample07(){
         return "thymeleafEx/thymeleafEx07";
     }
